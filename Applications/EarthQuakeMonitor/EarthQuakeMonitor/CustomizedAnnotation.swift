b27a3f8138cbc997a2f4edd950c2eb95b12e45c5
yxu0001@yahoo.com <yxu0001@yahoo.com>
2016-03-31 09:06:48 -0700

    Customized Annotation

---
+++ Applications/EarthQuakeMonitor/EarthQuakeMonitor/CustomizedAnnotation.swift
@@ -0,0 +1,13 @@
+//
+//  CustomizedAnnotation.swift
+//  EarthQuakeMonitor
+//
+//  Created by Yijia Xu on 3/30/16.
+//  Copyright © 2016 athenahealth. All rights reserved.
+//
+
+import UIKit
+
+class CustomizedAnnotation: NSObject {
+
+}
---
+++ Applications/EarthQuakeMonitor/EarthQuakeMonitor/CustomizedAnnotationView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomizedAnnotationView.swift
+//  EarthQuakeMonitor
+//
+//  Created by Yijia Xu on 3/30/16.
+//  Copyright © 2016 athenahealth. All rights reserved.
+//
+
+import UIKit
+
+class CustomizedAnnotationView: MKAnnotationView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
